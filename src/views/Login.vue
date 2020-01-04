967b63d58a44412c1b9830268787b3eaa57594ef
dovahkiin16 <edenfernandezjr@gmail.com>
2020-01-04 10:22:43 +0800

    Fixes

---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container class="login-container">
+        <v-row align="center" justify="center">
+            <v-col cols="4">
+                <v-card raised>
+                    <v-card-title>Expense Calculator</v-card-title>
+                    <v-divider/>
+                    <v-card-text>
+
+                    </v-card-text>
+                    <v-divider/>
+                    <v-card-actions>
+                        <v-btn color="primary">Login</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'login',
+    }
+</script>
+
+<style scoped>
+    .login-container {
+        padding-top: 68px;
+    }
+</style>
