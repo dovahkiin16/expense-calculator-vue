fe759645dfae0bceeb93f2ae81fdc4c5cd43df9f
dovahkiin16 <edenfernandezjr@gmail.com>
2020-01-07 04:31:32 +0800

    Add login persistence

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 import login from './login.store'
 import user from './user.store'
 
 Vue.use(Vuex)
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 export default new Vuex.Store({
   state: {},
@@ -12,5 +17,6 @@ export default new Vuex.Store({
   modules: {
     login,
     user,
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
