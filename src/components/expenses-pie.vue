b856ec936bcf315e79aa53867a06d1799c7fe064
dovahkiin16 <edenfernandezjr@gmail.com>
2020-01-19 18:38:39 +0800

    yes

--- src/components/expenses-pie.vue
+++ src/components/expenses-pie.vue
@@ -9,13 +9,13 @@
     data() {
       return {
         categories: [
-          {text: 'Food', value: 'food', color: '#81C784'},
-          {text: 'Transportation', value: 'transportation', color: '#FFF176'},
           {text: 'Loan', value: 'loan', color: '#E57373'},
-          {text: 'Rent', value: 'rent', color: '#BA68C8'},
+          {text: 'Food', value: 'food', color: '#81C784'},
           {text: 'Utility', value: 'utility', color: '#DCE775'},
+          {text: 'Rent', value: 'rent', color: '#BA68C8'},
+          {text: 'Transportation', value: 'transportation', color: '#FFF176'},
+          {text: 'Home and Lifestyle', value: 'new item', color: '#4DD0E1'},
           {text: 'Toiletries', value: 'toiletries', color: '#7986CB'},
-          {text: 'New Item', value: 'new item', color: '#4DD0E1'},
         ]
       }
     },
